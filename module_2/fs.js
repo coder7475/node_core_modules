23b3d4fc8d753ce285d1b214b08ad58e92fa9557
coder7475 <robiulhossain7475@gmail.com>
2025-05-29 20:15:14 +0600

    feat: learn async file read

--- module_2/fs.js
+++ module_2/fs.js
@@ -1,10 +1,27 @@
-// file read - I/O intensive task
+// file read/write - I/O intensive task
 // synchronous
 // 2. file read -> single thread -> block the event loop
 const fs = require('node:fs');
 
+// reading from file
 const data = fs.readFileSync("./files/hello.txt", { encoding: 'utf-8' });
 
 console.log(data);
+
+// witing to a file
+const text = "Writing to a file second time";
+
+fs.writeFileSync("./files/hello.txt", text);
+
+
+
 // asynchronousk
 // file read -> single thread -> event loop -> thread pool
+const data_2 = fs.readFile('./files/hello.txt', { encoding: 'utf-8' }, (err, data) => {
+  if (err) {
+    console.log("Something went wrong!", err);
+    return;
+  }
+
+  console.log(data);
+})
