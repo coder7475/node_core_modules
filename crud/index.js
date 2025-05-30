6455a5dffe49d56ed3e8fd0c8e5eeb6b59d2cbc1
coder7475 <robiulhossain7475@gmail.com>
2025-05-30 21:20:03 +0600

    feat: todos routing

--- crud/index.js
+++ crud/index.js
@@ -1,9 +1,22 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  res.end("Welcome to CRUD APP!");
+  // res.end("Welcome to CRUD APP!");
+  if (req.url === "/todos" && req.method === "GET") {
+    res.end("All Todos")
+  } else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    res.end("Todos Created");
+  } else {
+    res.end("Route Not found");
+  }
 });
 
 server.listen(5000, "localhost", () => {
   console.log("Server listening to port 5000");
 })
+
+/**
+ * /todos - GET - All todos
+ * /todos/create-todo - POST . Create Todo
+ *
+ */
