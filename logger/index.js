e515ae7bf3f6a0be31ae57383d27a485688d33d0
coder7475 <robiulhossain7475@gmail.com>
2025-05-30 20:07:03 +0600

    feat: build a logger

--- logger/index.js
+++ logger/index.js
@@ -1,24 +1,26 @@
 // console.log(process.argv);
 const path = require('path');
 const fs = require('fs');
+const { timeStamp } = require('console');
 
 const inputArgs = process.argv.slice(2)
 
-const text = inputArgs.join(" ");
-
+const text = inputArgs.join(" ").concat("\n");
+const timestamp = new Date().toISOString();
+console.log(timestamp);
 if (!text) {
   console.log("X Please provide a message to log");
   console.log("Ex: node logger/ Hello World!");
   process.exit(1);
 }
-
-console.log("Read Arguments");
+const log = `${text} ${timestamp}\n`;
+//console.log("Read Arguments");
 
 const filePath = path.join(__dirname, "log.txt");
+//console.log(text);
+//console.log(filePath);
 
-console.log(filePath);
-
-fs.appendFile(filePath, text, { encoding: 'utf-8' }, (err) => {
+fs.appendFile(filePath, log, { encoding: 'utf-8' }, (err) => {
   if (err) throw err;
   console.log("Finished writing!");
 })
